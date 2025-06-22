42a3de349dfe2c5232dc5450ad9e93a9b2919f5b
mahred1 <mahred.kemal@gmail.com>
2025-06-22 10:44:40 +0300

    integrated customer account creation

--- src/features/customers/CreateCustomer.jsx
+++ src/features/customers/CreateCustomer.jsx
@@ -1,32 +1,43 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createAccount } from "./customerSlice";
 
 function Customer() {
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
+  const dispatch = useDispatch();
+  const customer = useSelector((store) => store.customer);
 
-  function handleClick() {}
+  function handleClick() {
+    if (fullName === "" || nationalId === "") return;
+    dispatch(createAccount(fullName, nationalId));
+    setFullName("");
+    setNationalId("");
+  }
 
   return (
-    <div>
-      <h2>Create new customer</h2>
-      <div className="inputs">
-        <div>
-          <label>Customer full name</label>
-          <input
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-          />
+    (customer.fullName === "" || customer.nationalId === "") && (
+      <div>
+        <h2>Create new customer</h2>
+        <div className="inputs">
+          <div>
+            <label>Customer full name</label>
+            <input
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>National ID</label>
+            <input
+              value={nationalId}
+              onChange={(e) => setNationalId(e.target.value)}
+            />
+          </div>
+          <button onClick={handleClick}>Create new customer</button>
         </div>
-        <div>
-          <label>National ID</label>
-          <input
-            value={nationalId}
-            onChange={(e) => setNationalId(e.target.value)}
-          />
-        </div>
-        <button onClick={handleClick}>Create new customer</button>
       </div>
-    </div>
+    )
   );
 }
 
